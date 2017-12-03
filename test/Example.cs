1d1bfc8fc193a4212273caba118fca77cb8ef430
Markus Hjort <markus.hjort@gmail.com>
2017-12-03 19:36:05 +0200

    Add example

---
+++ test/Example.cs
@@ -0,0 +1,23 @@
+﻿using AivoTree;
+
+namespace Test
+{
+    public class Example
+    {
+        public static TreeNode<BotContext> createCounterAttackBehaviour()
+        {
+            var enoughManaToDeployUnit = new ActionNode<BotContext>((timeTick, ctx) => AivoTreeStatus.Success);
+            var lessUnitsThanOpponent = new ActionNode<BotContext>((timeTick, ctx) => AivoTreeStatus.Success);
+            var bestTargetToAttack = new ActionNode<BotContext>((timeTick, ctx) => AivoTreeStatus.Success);
+            var bestUnitToAttack = new ActionNode<BotContext>((timeTick, ctx) => AivoTreeStatus.Success);
+            var deploySelectedUnit = new ActionNode<BotContext>((timeTick, ctx) => AivoTreeStatus.Success);
+
+            return new SelectorNode<BotContext>(
+                new SequenceNode<BotContext>(enoughManaToDeployUnit, lessUnitsThanOpponent, bestTargetToAttack,
+                    bestUnitToAttack, deploySelectedUnit));
+        }
+    }
+
+
+    public class BotContext{}
+}
